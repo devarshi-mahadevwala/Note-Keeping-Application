f6a3454a762b8c08a132d1bfb35507b79a29b739
Devarshi <ui19co17@iiitsurat.ac.in>
2023-04-13 17:09:00 +0530

    app.js hashrouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { createHashRouter,ReactProvider } from 'react-router-dom';
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReactProvider router={router} />
   </React.StrictMode>
 );
 
